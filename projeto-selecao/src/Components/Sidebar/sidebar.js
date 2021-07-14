b0f048fd9c47b0ba0d1a14223641a1c2a7f7e204
Marcus Vinicius Monteiro <48728730+17102520@users.noreply.github.com>
2021-07-13 23:50:36 -0300

    commit atualização dashboard

--- projeto-selecao/src/Components/Sidebar/sidebar.js
+++ projeto-selecao/src/Components/Sidebar/sidebar.js
@@ -6,10 +6,6 @@ const Sidebar = ({ sidebar }) => (
 
     <div className="conteiner">
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet" />
-
         <div className="sidebar">
             <div>
                 <img src={logo} alt="Foto do perfil" className="sidebar-avatar__image" />
