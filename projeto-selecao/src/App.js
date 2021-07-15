d8c749797eafa17f12b4f2541b632075eca5bea7
Marcus Vinicius Monteiro <48728730+17102520@users.noreply.github.com>
2021-07-15 02:27:59 -0300

    commit atualização da parte de atividades

--- projeto-selecao/src/App.js
+++ projeto-selecao/src/App.js
@@ -2,26 +2,38 @@ import React from 'react'
 import './App.css';
 
 import Sidebar from './Components/Sidebar/sidebar'
+import Activities from './Components/Activities/activities';
 
-
-const App = () => {
+const App = () => { 
 
   const sidebar = {
-    "id": 1,
     "name": "Fausto Silva",
-    "curse": "Ciência da Computação",
+    "course": "Ciência da Computação",
     "dashboard": "Dashboard",
-    "myarea": "Meus cursos"
+    "myarea": "Meus cursos",
+    "image": "https://i.imgur.com/aU48TWI.jpg",
   };
 
-  
+  const activities = {
+    "id": "1",
+    "resume": "Resumo",
+    "next": "Próximas Atividades",
+    "subject": "Algoritmos e Programação",
+    "task": "Enviar arquivo Peter Smokes",
+    "image": "https://i.imgur.com/aadwyfC.jpg",
+    "deadline": "10/10/20",
+    "enrolledCourses": "2",
+    "nextActivities": "2",
+    "onlineStudents": "785",
+  };
 
   return (
     <div className="App">
-      <Sidebar
-        sidebar={sidebar}
-      />
+      <Sidebar sidebar={sidebar} />
+      <Activities activities={activities}/>
     </div>
+
+
   );
 }
 
--- projeto-selecao/src/Components/Sidebar/sidebar.js
+++ projeto-selecao/src/Components/Sidebar/sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './Ellipse 1.png';
-import './sidebar.css'
+import './sidebar.css';
 
 const Sidebar = ({ sidebar }) => (
 
@@ -8,9 +7,9 @@ const Sidebar = ({ sidebar }) => (
 
         <div className="sidebar">
             <div>
-                <img src={logo} alt="Foto do perfil" className="sidebar-avatar__image" />
+                <img src={sidebar.image} alt="Foto do perfil" className="sidebar-avatar__image" />
                 <h1 className="sidebar-avatar__name">{sidebar.name}</h1>
-                <p className="sidebar-avatar__curse">{sidebar.curse}</p>
+                <p className="sidebar-avatar__course">{sidebar.course}</p>
             </div>
             <div className="menu">
                 <h1 className="sidebar-menu__dashboard">{sidebar.dashboard}</h1>
