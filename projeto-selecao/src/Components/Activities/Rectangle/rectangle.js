328d674711a808fb126f38457b443250ce3adb09
Marcus Vinicius Monteiro <48728730+17102520@users.noreply.github.com>
2021-07-15 17:12:30 -0300

    commit Help Marcelin

--- projeto-selecao/src/Components/Activities/Rectangle/rectangle.js
+++ projeto-selecao/src/Components/Activities/Rectangle/rectangle.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './rectangle.css';
 import {FaGraduationCap} from 'react-icons/fa';
 
-const Rectangle = () => (
+const Rectangle = ({activities}) => (
 
     <div className="rectangle"> 
-        <h1 className="activities-rectangle__quantity">02</h1>
+        <h1 className="activities-rectangle__quantity">{activities.enrolledCourses}</h1>
         <p className="activities-rectangle__enrolledCourses">Cursos Matriculados</p>
         <FaGraduationCap className="activities-rectangle__icon"/>
     </div>
